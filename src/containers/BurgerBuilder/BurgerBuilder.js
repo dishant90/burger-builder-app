c79af2b80f4cf3e6ee21a1a64341b27d82bbbe24
dishant90 <kalradishant03@gmail.com>
2020-08-12 14:13:38 +0530

    Add Ingredients functionality

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,22 +4,50 @@ import Aux from '../../hoc/Auxiliary'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    bacon: 0.7,
+    cheese: 0.4,
+    meat: 0.8
+}
+
 class BurgerBuilder extends Component {
 
     state = {
         ingredients: {
-            salad: 2,
+            salad: 0,
             bacon: 0,
-            cheese: 2,
+            cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const newCount = oldCount + 1;
+        const oldPrice = this.state.totalPrice;
+        const updatedIngredients = {...this.state.ingredients};
+        updatedIngredients[type] = newCount;
+        const newPrice = oldPrice + INGREDIENT_PRICES[type]
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: newPrice
+        })
+        console.log('Old quantity ' + oldCount + ' of ingredient ' + type)
+        console.log('New price: ' + newPrice)
+        console.log('New quantity ' + newCount + ' of ingredient ' + type)
+    }
+
+    removeIngredientHandler(type) {
+
     }
 
     render() {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls addIngredient={this.addIngredientHandler} />
             </Aux>
         );
     }

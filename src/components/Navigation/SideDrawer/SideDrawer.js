e85d415190131215bb9795b485f319312499921c
dishant90 <kalradishant03@gmail.com>
2020-09-07 12:23:30 +0530

    Adding side drawer and placing backdrop when opening the side drawer

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import NavigationItems from '../NavigationItems/NavigationItems'
+import classes from './SideDrawer.module.css'
+import Logo from '../../ui/Logo/Logo'
+import Backdrop from '../../ui/Backdrop/Backdrop'
+import Aux from '../../../hoc/Auxiliary'
+
+const sideDrawer = (props) => {
+
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
+    return(
+        <Aux>
+            <Backdrop show={props.open} hide={props.close} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+        
+    )
+}
+
+export default sideDrawer;
